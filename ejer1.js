6175069ce17ca2736992c1f4ab65132a4f1ae834
BertoniPablo <bertonipablo3@gmail.com>
2024-04-23 19:49:17 -0300

    primeros dos ejers

---
+++ ejer1.js
@@ -0,0 +1,6 @@
+const num1 = 6
+const num2 = 8
+
+const suma = num1 + num2
+
+console.log("la suma es: " + suma)
---
+++ ejer2.js
@@ -0,0 +1,9 @@
+const num1 = 20
+const num2 = 25
+const num3 = 30
+
+const mayor = Math.max(num1, num2, num3)
+const menor = Math.min(num1, num2, num3)
+
+console.log("el numero mayor es: " + mayor)
+console.log("el numero menor es: " + menor)
